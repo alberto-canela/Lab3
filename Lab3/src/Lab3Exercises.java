f3ec3204f68a867a903c0f2d1eff916c8873c337
4015a4 <4015a4@corazon.ece.uprm.edu>
2017-11-15 12:43:59 -0400

    Change Jframe name, set size to 200*200, put Jframe on center of screen.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,20 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Alberto Canela");
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Getting Screen Dimensions to put on program on center.
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = screenSize.getWidth();
+		double height = screenSize.getHeight();
+		
+		myFrame.setLocation((int)Math.ceil(width/2-100), (int)Math.ceil(height/2-100));
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
